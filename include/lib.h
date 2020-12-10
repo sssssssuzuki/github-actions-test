2ae37396369b215f0ad30ccfe722ffe2364a9872
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2020-12-10 18:12:51 +0900

    init

---
+++ include/lib.h
@@ -0,0 +1,22 @@
+﻿// File: autd3_c_api.h
+// Project: include
+// Created Date: 07/02/2018
+// Author: Shun Suzuki
+// -----
+// Last Modified: 10/12/2020
+// Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
+// -----
+// Copyright (c) 2018-2020 Hapis Lab. All rights reserved.
+//
+
+#pragma once
+
+#if WIN32
+#define EXPORT __declspec(dllexport)
+#else
+#define EXPORT __attribute__((visibility("default")))
+#endif
+
+extern "C" {
+EXPORT void SayHello();
+}
---
+++ lib/lib.cpp
@@ -0,0 +1,26 @@
+// File: sequence.cpp
+// Project: lib
+// Created Date: 01/07/2020
+// Author: Shun Suzuki
+// -----
+// Last Modified: 10/12/2020
+// Modified By: Shun Suzuki (suzuki@hapis.k.u-tokyo.ac.jp)
+// -----
+// Copyright (c) 2020 Hapis Lab. All rights reserved.
+//
+
+#include "lib.h"
+
+#include <iostream>
+
+#if (_WIN32 || _WIN64)
+#define OS_STR ("Windows")
+#elif defined __APPLE__
+#define OS_STR ("macOS")
+#elif defined __linux__
+#define OS_STR ("linux")
+#else
+#error "Not supported."
+#endif
+
+void SayHello() { std::cout << "Hello, " << OS_STR << "!" << std::endl; }
